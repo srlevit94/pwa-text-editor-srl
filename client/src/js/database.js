7596912659a16b5b59aacdad782c2234e7efe2bd
shanelevites <srlevit94@gmail.com>
2023-03-07 23:26:03 -0800

    editor loses focus, need to figure out install,

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,10 +15,10 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) =>{
   console.log('ADD to the database');
-  const textsDb = await openDB('jate', 1);
-  const tx = textsDb.transaction('jate', 'readwrite');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.add({ text: content });
+  const request = store.add({ jate: content });
   const result = await request;
   console.log('🚀 - data saved to the database', result);
   //console.error('putDb not implemented');
@@ -27,8 +27,8 @@ export const putDb = async (content) =>{
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET all from the database');
-  const textsDb = await openDB('jate', 1);
-  const tx = textsDb.transaction('jate', 'readonly');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
   const request = store.getAll();
   const result = await request;
